6d4b200ac80e87622095b13796b6e8a6b895bf37
breno morais <breno.lopes.morais@hotmail.com>
2018-07-15 00:08:48 -0300

    alterar nls_lang do oracle (idioma-linguagem)

---
+++ OracleOne/consultar-e-alterar-lingua-default.sql
@@ -0,0 +1,17 @@
+-- Query consultar qual nls_lang (linguagem-idioma) está sendo usada pelo Oracle,
+-- idiomamportante saber essa informação, para trabalhar com datas.
+
+show parameter nls_lang;
+
+-- Caso, após a consulta o nls_lang, esteja em outra linguagem que não seja: portugues-brasil
+-- e possivel, alterar o nls_lang com os comandos abaixo: altera linguagem, país, caracteres e
+-- formato de data.
+
+Alter Session Set nls_language='BRAZILIAN PORTUGUESE';
+Alter Session Set NLS_TERRITORY = 'BRAZIL';
+Alter Session Set NLS_NUMERIC_CHARACTERS=',.';
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY HH24:MI:SS';
+
+--Query que permite verificar todos as linguagem aceitas pelo Oracle
+select * from v$nls_parameters;
+
